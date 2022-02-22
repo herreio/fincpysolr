463bbc9dcbf65099a465c1f0fd37a308dc6a701b
herre <donatus.herre@slub-dresden.de>
2022-02-22 14:15:05 +0100

    Update fields in setup

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setuptools.setup(
     description="Access finc Solr documents.",
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
-    license=open("LICENSE").read(),
+    license="GPLv3",
     url="https://github.com/herreio/fincpysolr",
     packages=["fincpysolr"],
-    install_requires=["vupysolr @ git+https://github.com/herreio/vupysolr@main"],
+    install_requires=["vupysolr"],
 )
