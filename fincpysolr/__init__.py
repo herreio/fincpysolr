3151fa15e6c364b96571846cb5da446f2030ff9d
Donatus Herre <donatus.herre@slub-dresden.de>
2022-05-25 01:44:20 +0200

    Bump version number

--- fincpysolr/__init__.py
+++ fincpysolr/__init__.py
@@ -5,7 +5,7 @@ For more information on finc, see https://finc.info.
 """
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 from .finc_ai import FincAI
 from .finc_main import FincMain
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="fincpysolr",
-    version="0.1.7",
+    version="0.1.8",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="Access finc Solr documents.",
