438fc0e11d5c30ad2cf26c6e3d2770fef8018fba
Donatus Herre <donatus.herre@slub-dresden.de>
2023-10-24 23:49:58 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="fincpysolr",
-    version="0.1.15",
+    version="0.2.0",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="Access finc Solr documents.",
