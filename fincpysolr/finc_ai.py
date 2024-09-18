3eaeb9c322fcd42d0c464c1a2bb8335f0d963ff5
Donatus Herre <donatus.herre@slub-dresden.de>
2024-09-18 10:37:58 +0200

    Adjust class FincAI

--- fincpysolr/finc_ai.py
+++ fincpysolr/finc_ai.py
@@ -1,35 +1,35 @@
+import json
 import logging
 
 from .base import FincIndex
 from .docs import FincParser
 
-from vupysolr.utils import json_req
-
 
 class FincAI(FincIndex):
 
-    def __init__(self, domain, institution, loglevel=logging.WARNING):
+    def __init__(self, domain, institution, core="biblio", name="finc-ai", loglevel=logging.WARNING):
         self.domain = domain.strip("/")
-        self.url_blob = "{0}/blob".format(self.domain)
-        super().__init__(url="{0}/solr".format(self.domain), core="biblio", name="finc-ai", institution=institution, marc=False, loglevel=loglevel)
+        super().__init__(url="{0}/solr".format(self.domain), core=core, name=name, institution=institution, marc=False, loglevel=loglevel)
 
-    def get(self, id, blob=False):
-        document = self._get(id)
-        if blob and document is not None:
-            if "fullrecord" in document:
-                fullrec_id = document["fullrecord"]
-                if type(fullrec_id) == str and fullrec_id.startswith("blob:"):
-                    blob_id = fullrec_id.split(":")[1]
-                    fullrecord = self._get_blob(blob_id)
-                    if fullrecord is not None:
-                        document["fullrecord"] = fullrecord
+    def get(self, id, ims=True):
+        if ims:
+            document = self._get(id, post=self.decode_ims)
+        else:
+            document = self._get(id)
         if self.check_institution(document):
-            return FincParser(document, self.institution, marc=False, ai=blob)
+            return FincParser(document, self.institution, marc=False, ai=True)
 
-    def get_blob_url(self, id):
-        return "{0}?{1}".format(self.url_blob, id)
-
-    def _get_blob(self, id):
-        self.logger.info("Fetch fullrecord from blob store of Solr index finc-ai.")
-        url = self.get_blob_url(id)
-        return json_req(url)
+    def decode_ims(self, document):
+        if "fullrecord" in document and (
+            "recordtype" in document and document["recordtype"] == "is" or
+            "record_format" in document and document["record_format"] == "is"):
+            try:
+                document["fullrecord"] = json.loads(document["fullrecord"])
+            except json.JSONDecodeError as err:
+                doc_id = None
+                if "id" in document:
+                    doc_id = document["id"]
+                self.logger.exception(err)
+                if doc_id is not None:
+                    self.logger.error("Parsing intermediate data for document with id '{0}' failed!".format(doc_id))
+        return document
